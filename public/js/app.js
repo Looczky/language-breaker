c44e05a87091acb7b24a7dde200c76a38823f2f0
Looczky <kwiatkowskiluk@protonmail.com>
2024-02-08 22:56:13 +0100

    changing words at click

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,74 @@ window.onload = async () => {
         return text;
     }
 
-    function getRandomValue(){
-        return data[(Math.floor(Math.random()*data.length))];
+    function getRandomAndDelete(pool){
+        const rand = Math.floor(Math.random()*pool.length)
+        return pool.splice(rand,1)[0];
+    }
+
+    function setValues(package){
+        target.textContent = package[i][0]
+    
+        let correctPos = Math.floor(Math.random()*3)
+        if (correctPos === 0) {
+            guess1.textContent = package[i][1]; // Value for correct guess position
+            guess2.textContent = package[i][2];
+            guess3.textContent = package[i][3];
+        } else if (correctPos === 1) {
+            guess1.textContent = package[i][2];
+            guess2.textContent = package[i][1]; // Value for correct guess position
+            guess3.textContent = package[i][3];
+        } else {
+            guess1.textContent = package[i][2];
+            guess2.textContent = package[i][3];
+            guess3.textContent = package[i][1]; // Value for correct guess position
+        }
     }
 
     const data = await getData();
-    console.log(getRandomValue());
+    const lines = data.split('\n');
+
+    let linesPool = [...lines];
+    let package = [];
+    let wordsCount = 20;
+
+    for (let i = 0; i<wordsCount; i++){
+
+        randomValue1 = getRandomAndDelete(linesPool).split('\t');
+        const [spanish, english] = randomValue1;
+        
+        randomValue2 = getRandomAndDelete(linesPool).split('\t');
+        randomValue3 = getRandomAndDelete(linesPool).split('\t');
+        const [, wrong_1] = randomValue2;
+        const [, wrong_2] = randomValue3;
+
+        package.push([spanish,english,wrong_1,wrong_2]);
+    }
+
+    let i = 0
+
+    const target = document.querySelector('#target')
+    const guess1 = document.querySelector('#guess1')
+    const guess2 = document.querySelector('#guess2')
+    const guess3 = document.querySelector('#guess3')
+
+    
+    await setValues(package)
+
+
+    document.querySelectorAll('.link').forEach(link =>{
+        link.addEventListener('click',()=>{
+            if (i < wordsCount){
+                i++;
+                console.log('clicked!');
+                
+                setValues(package);
+            }
+        })
+    })
+    
+
+    // console.log(lines);
     
     
 };
